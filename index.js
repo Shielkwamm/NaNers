237ccf85485d810a1b88ad328dc9c34e5e509cc5
neobii <iwilldevelopcodeforyou@gmail.com>
2021-04-15 03:25:36 -0500

    query for current room, attempt to add messages +🦓

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 const https = require('https');
-const fetch = require("node-fetch");
 
 require('dotenv').config();
+const { request, gql, GraphQLClient } = require('graphql-request')
 
 const client = new Discord.Client();
 
@@ -12,12 +12,47 @@ client.once('ready', () => {
   console.log("Online")
 })
 
+var rooms;
+
+async function main() {
+  const endpoint = 'https://one-zork.herokuapp.com/graphql'
+  const graphQLClient = new GraphQLClient(endpoint, {
+    Headers: {
+      apiKey: process.env.VULCAN,
+    },
+  })
+  const query = gql`
+    {
+      rooms {
+        results {
+          _id
+          name
+        }
+      }
+    }
+  `
+
+  const data = await graphQLClient.request(query)
+  //const roomsData = JSON.stringify(data, undefined, 2);
+  rooms = data.rooms.results;
+}
+
+main().catch((error) => console.error(error))
+
+
 client.on('message', message => {
+  let room = rooms.find((room) => room.name === message.channel.name)
+
+  insertMessage(room._id, message.author.username + " : " + message.content);
+
   if(!message.content.startsWith(prefix) || message.author.bot) return;
   const args = message.content.slice(prefix.length).split(/ +/);
   const command = args.shift();
 
-  console.log(args, command);
+  if(command === 'bye') {
+    message.channel.send("bye");
+    client.destroy();
+  }
 
   if(command === 'getRandomCatFact') {
     const req = https.get('https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1', (res) => {
@@ -34,4 +69,34 @@ client.on('message', message => {
   }
 })
 
-client.login(process.env.DISCORD);
+client.login(process.env.DISCORD); 
+
+async function insertMessage(roomId, message) {
+  const endpoint = 'https://one-zork.herokuapp.com/graphql'
+  const graphQLClient = new GraphQLClient(endpoint, {
+    Headers: {
+      apiKey: process.env.VULCAN,
+    },
+  });
+  const variables = {
+    createMessageInput: {
+      data: {
+        roomId: roomId,
+        text: message
+      }
+    }
+  }
+  const mutation = gql`
+    mutation CreateMessageMutation($createMessageInput: CreateMessageInput) {
+      createMessage(input: $createMessageInput) {
+        data {
+          roomId,
+          text
+        }
+      }
+    }
+  `;
+
+  const results = await graphQLClient.request(mutation, variables);
+  console.log(results);
+}
